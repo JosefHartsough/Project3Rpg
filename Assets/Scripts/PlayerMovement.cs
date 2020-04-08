44caa82d690fb962055d231bb5543e2c7f11cf4a
Josef Hartsough <hartsouj@mail.gvsu.edu>
2020-04-08 13:09:24 -0400

    added comments, fixed issues with player states. Still having animation issue with player turning back to the idle state..... cant figure it out yet.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-//This is a test
+// TODO: Implement different states. Still need to work on:death, attack1, attack2, itemUse1,itemUse2.
+// Enum for our players state. (This is a state machine)
 public enum StateOfPlayer
 {
     walk,
@@ -11,6 +11,14 @@ public enum StateOfPlayer
     interact
 
 }
+
+// Main class to manage our players movement
+/* <summary>
+ * Has all the variables for our player. 
+ * <function> void Start() -This function is called everytime we start
+ * <function> void Update() -This function is calle every frame
+ * </summary>
+ */
 public class PlayerMovement : MonoBehaviour
 {
     // Varaible for state of player
@@ -33,6 +41,8 @@ public class PlayerMovement : MonoBehaviour
         playerState = StateOfPlayer.walk;
         playerAnimations = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody2D>();
+        playerAnimations.SetFloat("moveX", 0);
+        playerAnimations.SetFloat("moveY", -1);
     }
 
     // Update is called once per frame
@@ -44,8 +54,9 @@ public class PlayerMovement : MonoBehaviour
         changeInPosition.y = Input.GetAxis("Vertical");
         if (Input.GetButtonDown("attack") && playerState != StateOfPlayer.attack) 
         {
-            StartCoroutine(AttackCo());
+            StartCoroutine(animationCoroutine());
         }
+        // Checks if our current state is walk, make sures the animation is set to move
         else if(playerState == StateOfPlayer.walk)
         {
             
@@ -53,8 +64,9 @@ public class PlayerMovement : MonoBehaviour
         }
         UpdateAnimationAndMovement();
     }
-
-    private IEnumerator AttackCo()
+    // These are called Coroutines. They are used for animations.
+    // Creates a small delay between the attack animations and walking.
+    private IEnumerator animationCoroutine()
     {
         playerAnimations.SetBool("attacking", true);
         playerState = StateOfPlayer.attack;
@@ -62,9 +74,15 @@ public class PlayerMovement : MonoBehaviour
         yield return null;
         // Leave false, otherwise our player goes back into the movement state
         playerAnimations.SetBool("attacking", false);
+        // Waits an additional 3 seconds and sets our state back to walk for a more fluid animation
         yield return new WaitForSeconds(0.3f);
         playerState = StateOfPlayer.walk;
     }
+    /* <summary>
+     * Changes the variables set in Unity to change our X
+     * position and our y position to our variables. 
+     * </summary>
+     */
     void UpdateAnimationAndMovement()
     {
         if (changeInPosition != Vector3.zero)
@@ -83,6 +101,7 @@ public class PlayerMovement : MonoBehaviour
     // Function for player movement. Change speed variable in Unity
     void MoveCharacter()
     {
+        changeInPosition.Normalize();
         rigidBody.MovePosition(transform.position + changeInPosition * speed * Time.deltaTime);
     }
 }
