2d36a14fd1946c5ec6a7ace58ff1281d1a5aad5c
shinjimasuka <collierj@mail.gvsu.edu>
2020-04-12 15:26:08 -0400

    Formatted the enemy class correctly now that I know how to do everything I need to do. I am going to start pathfinding.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,7 +7,8 @@ using UnityEngine;
 // Enum for our enemys state. (This is a state machine)
 public enum StateOfEnemy
 {
-    walk,
+    idle,
+    moving,
     attack,
     interact
 
@@ -34,16 +35,23 @@ public class EnemyMovement : MonoBehaviour
     // Vector for how much enemys position changes
     private Vector3 changeInPosition;
 
+    // position of enemy
+    private Vector3 enemy_position;
+
     // Variable to access the enemys animations
     private Animator enemyAnimations;
-    // Start is called before the first frame update
 
+    // Get the player sprite
+    private GameObject player;
+    private Vector3 player_position;
+
+    // Start is called before the first frame update
     void Start()
     {
-        enemyState = StateOfEnemy.walk;
+        enemyState = StateOfEnemy.idle;
         enemyAnimations = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody2D>();
-        enemyAnimations.SetBool("Running", true);
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
@@ -51,37 +59,46 @@ public class EnemyMovement : MonoBehaviour
     {
         // Every frame resets how much the enemy has changed
         changeInPosition = Vector3.zero;
-        // changeInPosition.x = 1;
-        changeInPosition.y = -2;
-        if (transform.position.y < -10 && transform.position.y > -15)
-        {
-            // changeInPosition.y = 0;
-            enemyAnimations.SetBool("Running", false);
-            StartCoroutine(animationCoroutine());
-        }
-        // Checks if our current state is walk, make sures the animation is set to move
-        if (enemyState == StateOfEnemy.walk)
-        {
-            UpdateAnimationAndMovement();
-            // enemyAnimations.SetBool("Run", true);
-            enemyAnimations.SetBool("Attacking", false);
-        }
-        Debug.Log("what is state: " + enemyState);
+        // reset the positions of the player and enemy
+        enemy_position = transform.position;
+        player_position = player.transform.position;
+        determineMove(player_position, enemy_position);
+        checkState();
         UpdateAnimationAndMovement();
     }
-    // These are called Coroutines. They are used for animations.
-    // Creates a small delay between the attack animations and walking.
-    private IEnumerator animationCoroutine()
-    {
-        enemyAnimations.SetBool("Attacking", true);
-        enemyState = StateOfEnemy.attack;
-        // Wait for 1 frame
-        yield return null;
-        // Leave false, otherwise our enemy goes back into the movement state
-        // enemyAnimations.SetBool("Attacking", false);
-        // Waits an additional 3 seconds and sets our state back to walk for a more fluid animation
-        yield return new WaitForSeconds(0.3f);
-        enemyState = StateOfEnemy.walk;
+
+    void determineMove(Vector3 player_position, Vector3 enemy_position){
+        Debug.Log("player poostin: " + player_position);
+        Debug.Log("enemy poostin: " + enemy_position);
+    }
+
+    void checkState(){
+        // Checks if our current state is moving, make sures the animation is set to move
+        switch (enemyState){
+            case StateOfEnemy.idle:
+                Debug.Log("state is idle");
+                UpdateAnimationAndMovement();
+                enemyAnimations.SetBool("Attacking", false);
+                enemyAnimations.SetBool("Running", false);
+                break;
+
+            case StateOfEnemy.moving:
+                Debug.Log("state is moving");
+                UpdateAnimationAndMovement();
+                enemyAnimations.SetBool("Running", true);
+                break;
+
+            case StateOfEnemy.attack:
+                Debug.Log("state is attacking");
+                enemyAnimations.SetBool("Attacking", true);
+                StartCoroutine(animationCoroutine());
+                UpdateAnimationAndMovement();
+                break;
+
+            default:
+                Debug.Log("Default case");
+                break;
+        }
     }
     /* <summary>
      * Changes the variables set in Unity to change our X
@@ -93,7 +110,6 @@ public class EnemyMovement : MonoBehaviour
         if (changeInPosition != Vector3.zero)
         {
             MoveCharacter();
-
         }
         else
         {
@@ -105,5 +121,17 @@ public class EnemyMovement : MonoBehaviour
     {
         changeInPosition.Normalize();
         rigidBody.MovePosition(transform.position + changeInPosition * speed * Time.deltaTime);
+    }
+    // These are called Coroutines. They are used for animations.
+    // Creates a small delay between the attack animations and moving.
+    private IEnumerator animationCoroutine()
+    {
+        // Wait for 1 frame
+        yield return null;
+        // Leave false, otherwise our enemy goes back into the movement state
+        // enemyAnimations.SetBool("Attacking", false);
+        // Waits an additional 3 seconds and sets our state back to moving for a more fluid animation
+        yield return new WaitForSeconds(0.3f);
+        enemyState = StateOfEnemy.idle;
     }
 }
