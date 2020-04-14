4a01140e43d337756e52ff55c417e858b2ce7c2a
shinjimasuka <collierj@mail.gvsu.edu>
2020-04-13 23:15:51 -0400

    Added in joe's stuff and now the scenes work.

---
+++ Assets/Scripts/killCounter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Class to manage the killcount on the UI
+public class killCounter : MonoBehaviour
+{
+    // Array of our enemies. It will be
+    GameObject[] enemies;
+
+    // Text for displaying the remaining enemies
+    public Text enemyCountText;
+
+    void Update() {
+
+        enemies = GameObject.FindGameObjectsWithTag("enemy");
+        enemyCountText.text = "Enemies Remaining: " + enemies.Length.ToString();
+    }
+}
